52562e27da4ba952573fe264aa7594dbb80c0283
kostenkoLiza09 <kostaz063@gmail.com>
2025-04-14 17:25:52 +0200

    add fix classes

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,11 +1,13 @@
 import lombok.With;
 
+import java.time.Instant;
 import java.util.List;
 @With
 public record Order(
         String id,
         List<Product> products,
 
-        OrderStatus orderStatus
+        OrderStatus orderStatus,
+        Instant dataTime
 ) {
 }
