e209c92c89c17a9b4a3b3fa9f39530190bcc4a00
kostenkoLiza09 <kostaz063@gmail.com>
2025-04-14 12:24:02 +0200

    add Optional to ProductRepo methods and updated tests

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -10,16 +11,13 @@ public class ShopService {
     public Order addOrder(List<String> productIds, OrderStatus orderStatus) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
-            products.add(productToOrder);
-        }
-
+           Optional <Product> productToOrder = productRepo.getProductById(productId);
+           if(productToOrder.isEmpty()){
+               System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+               return null;
+           }
+           }
         Order newOrder = new Order(UUID.randomUUID().toString(), products, orderStatus);
-
         return orderRepo.addOrder(newOrder);
     }
 
